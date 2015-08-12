9fd8fda6235d2bf83ea95b8b02aaa9f4f78d9dd5
Mukul Tripathi <mukul.nifft@gmail.com>
2015-08-12 01:26:09 -0500

    Updated changes to incorporate Dev DB and Prod db. Support for MySql added.

--- src/main/java/com/mtcl/service/InitDbService.java
+++ src/main/java/com/mtcl/service/InitDbService.java
@@ -23,7 +23,7 @@ import com.mtcl.repository.UserRepository;
 @Service
 @Transactional
 public class InitDbService {
-	
+
 	@Autowired
 	private BlogRepository blogRepository;
 	@Autowired
@@ -35,45 +35,48 @@ public class InitDbService {
 
 	@PostConstruct
 	public void init() {
-		Role roleUser = new Role();
-		roleUser.setName("ROLE_USER");
-		roleRepository.save(roleUser);
-		
-		Role roleAdmin = new Role();
-		roleAdmin.setName("ROLE_ADMIN");
-		roleRepository.save(roleAdmin);
-		
-		User userAdmin = new User();
-		userAdmin.setName("admin");
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		userAdmin.setPassword(encoder.encode("admin"));
-		List<Role> roles = new ArrayList<Role>();
-		roles.add(roleAdmin);
-		roles.add(roleUser);
-		userAdmin.setRoles(roles);
-		userAdmin.setEnabled(true);
-		userRepository.save(userAdmin);
-		
-		Blog blogJavavids = new Blog();
-		blogJavavids.setName("JavaVids");
-		blogJavavids.setUrl("http://feeds.feedburner.com/javavids?format=xml");
-		blogJavavids.setUser(userAdmin);
-		blogRepository.save(blogJavavids);
-		
-		Item item1 = new Item();
-		item1.setBlog(blogJavavids);
-		item1.setTitle("First");
-		item1.setLink("http://www.javavids.com/");
-		item1.setPublishedDate(new Date());
-		itemRepository.save(item1);
-		
-		Item item2 = new Item();
-		item2.setBlog(blogJavavids);
-		item2.setTitle("Second");
-		item2.setLink("http://www.javavids.com/");
-		item2.setPublishedDate(new Date());
-		itemRepository.save(item2);
-		
-		
+
+		if (roleRepository.findOneByName("ROLE_ADMIN") == null) {
+
+			Role roleUser = new Role();
+			roleUser.setName("ROLE_USER");
+			roleRepository.save(roleUser);
+
+			Role roleAdmin = new Role();
+			roleAdmin.setName("ROLE_ADMIN");
+			roleRepository.save(roleAdmin);
+
+			User userAdmin = new User();
+			userAdmin.setName("admin");
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			userAdmin.setPassword(encoder.encode("admin"));
+			List<Role> roles = new ArrayList<Role>();
+			roles.add(roleAdmin);
+			roles.add(roleUser);
+			userAdmin.setRoles(roles);
+			userAdmin.setEnabled(true);
+			userRepository.save(userAdmin);
+
+			Blog blogJavavids = new Blog();
+			blogJavavids.setName("JavaVids");
+			blogJavavids.setUrl("http://feeds.feedburner.com/javavids?format=xml");
+			blogJavavids.setUser(userAdmin);
+			blogRepository.save(blogJavavids);
+
+			Item item1 = new Item();
+			item1.setBlog(blogJavavids);
+			item1.setTitle("First");
+			item1.setLink("http://www.javavids.com/");
+			item1.setPublishedDate(new Date());
+			itemRepository.save(item1);
+
+			Item item2 = new Item();
+			item2.setBlog(blogJavavids);
+			item2.setTitle("Second");
+			item2.setLink("http://www.javavids.com/");
+			item2.setPublishedDate(new Date());
+			itemRepository.save(item2);
+		}
+
 	}
 }
