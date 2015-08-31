6eed8f885483f2cef81f6444dc2e58d831a24459
Mukul Tripathi <mukul.nifft@gmail.com>
2015-08-30 23:05:01 -0500

    adding support for prod and dev db with config files.

--- src/main/java/com/mtcl/service/BlogService.java
+++ src/main/java/com/mtcl/service/BlogService.java
@@ -6,16 +6,12 @@ import org.springframework.stereotype.Service;
 import com.mtcl.entity.Blog;
 import com.mtcl.entity.User;
 import com.mtcl.repository.BlogRepository;
-import com.mtcl.repository.UserRepository;
 
 @Service
 public class BlogService {
 
 	@Autowired
 	private BlogRepository blogRepository;
-
-	@Autowired
-	private UserRepository userRepository;
 
 	public void save(Blog blog, User user) {
 		blog.setUser(user);
